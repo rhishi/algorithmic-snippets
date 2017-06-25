4e30137a4fbc5c42f1884abf9822b8fa527e8780
Rhishikesh Limaye <rhishi@gmail.com>
2017-06-25 12:00:29 -0700

    Added a few more simple tests of QuickSort
    Covering the base cases, empty array, one-element array, two-element
    array ascending and descending, and a small example array.

--- sorting_test.cc
+++ sorting_test.cc
@@ -4,7 +4,35 @@
 // -----------------------------------------------------------------
 // QuickSort tests
 
-TEST(QuickSortTest, Test1) {
+TEST(QuickSortTest, Empty) {
+    int array[] = { };
+    int size = 0;
+    QuickSort(array, size);
+    EXPECT_TRUE(IsSorted(array, size));
+}
+
+TEST(QuickSortTest, OneElement) {
+    int array[] = { 3 };
+    int size = 1;
+    QuickSort(array, size);
+    EXPECT_TRUE(IsSorted(array, size));
+}
+
+TEST(QuickSortTest, TwoElementsAscending) {
+    int array[] = { 7, 10 };
+    int size = 2;
+    QuickSort(array, size);
+    EXPECT_TRUE(IsSorted(array, size));
+}
+
+TEST(QuickSortTest, TwoElementsDescending) {
+    int array[] = { 33, 16 };
+    int size = 2;
+    QuickSort(array, size);
+    EXPECT_TRUE(IsSorted(array, size));
+}
+
+TEST(QuickSortTest, SmallExample) {
     int array[] = { 3, 4, 1, 4, 7, 6, 7, 8, 5 };
     int size = 9;
     QuickSort(array, size);
